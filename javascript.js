4bd73425692b6957958ee7ab957626b991b3d05d
Karan Mahajan <karanmahajan919@gmail.com>
2018-04-04 12:35:47 +0530

    Signed-off-by: Karan Mahajan <karanmahajan919@gmail.com>

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
-function loadDoc() {
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").load("https://talaikis.com/api/quotes/random/");
+    });
+});
+function sendDoc(){
+	document.getElementById("demo").innerHTML="hello";
+}
+
+//https://api.twitter.com/1.1/statuses/update.json
+/*
+function sendDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -9,3 +20,4 @@ function loadDoc() {
     xhttp.open("GET", "http://api.forismatic.com/api/1.0/", true);
     xhttp.send(); 
 }
+*/
---
+++ jsx.js
@@ -0,0 +1,12 @@
+ $(document).ready(function() {
+    $("#random").on("click", function(){
+
+      $("#demo").html("Here is the message");
+
+    });
+  });
+
+
+
+//https://talaikis.com/api/quotes/random/
+ 
