569bb7462806d8bda796c7aa17fd55ee3d358435
Karan Mahajan <karanmahajan919@gmail.com>
2018-04-04 19:20:31 +0530

    Signed-off-by: Karan Mahajan <karanmahajan919@gmail.com>

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
- $(document).ready(function() {
-    $("#random").on("click", function(){
+$(document).ready( function(){
+	let text = '';
+	let author = '';
+	function  getQuote(){
+		$.get('https://talaikis.com/api/quotes/random/', function(response){ 
+		text = "--"+ response.quote;
+		author = response.author;
+     	$("#quote").html(response.quote);
+     	$("#author").html("--"+response.author);
+     	});
+	}
 
-      $("#quote").html("Here is the message");
+			
+	//$("#tweet").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes/&related=freecodecamp&text=' + encodeURIComponent('"' + text + '" ' + author));
+	$("#tweet").attr("href", 'https://twitter.com/intent/tweet?text=' + text);
 
-    });
-  });
+	getQuote();
 
+	$("#random").click(getQuote);
+	//$("#tweet").click(tweet);
 
-
-//https://talaikis.com/api/quotes/random/
- 
+	
+	
+});
